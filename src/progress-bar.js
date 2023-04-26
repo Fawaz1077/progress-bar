92ebcced54619091d4556edcc6d19ac71c338aa7
Fawaz1077 <107772207+Fawaz1077@users.noreply.github.com>
2023-04-26 16:23:36 -0400

    some changes

--- src/progress-bar.js
+++ src/progress-bar.js
@@ -10,49 +10,52 @@ class ProgressBar extends LitElement {
   };
 
   static styles = css`
-    :host {
-      display: block;
-      text-align: center;
-    }
+  :host {
+    display: block;
+    text-align: center;
+  }
 
-    .progress-bar-wrapper {
-      display: flex;
-      justify-content: center;
-      transform: translateY(calc(45vh - 80px));
+  .progress-bar-wrapper {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    transform: translateY(calc(45vh - 80px));
+    width: 100%;
+  }
+
+  .progress-bar-inner {
+    border-radius: 5px;
+    width: 70%;
+    height: 60px;
+    background-color: #ddd;
+    margin-bottom: 10px;
+  }
+
+  .progress-bar-filled {
+    background: orange;
+    border-radius: 5px;
+    height: 60px;
+    margin-top: 1px;
+    animation: progress-bar-filled var(--speed-of-bar, 5s) linear forwards;
+  }
+
+  @keyframes progress-bar-filled {
+    0% {
+      width: 0%;
+    }
+    100% {
       width: 100%;
     }
+  }
 
-    .progress-bar-inner {
-      border-radius: 5px;
-      width: 70%;
-      height: 60px;
-      background-color: #ddd;
-      margin-bottom: 10px;
-    }
+  .counter {
+    position: absolute;
+    top: 20px;
+    left: 87%;
+    font-size: 20px;
+  }
+`;
 
-    .progress-bar-filled {
-      background: orange;
-      border-radius: 5px;
-      height: 60px;
-      margin-top: 1px;
-      animation: progress-bar-filled var(--speed-of-bar, 5s) linear forwards;
-    }
-
-    @keyframes progress-bar-filled {
-      0% {
-        width: 0%;
-      }
-      100% {
-        width: 100%;
-      }
-    }
-
-    .counter {
-      position: relative;
-      top: -60px;
-      font-size: 20px;
-    }
-  `;
 
   constructor() {
     super();
@@ -64,9 +67,7 @@ class ProgressBar extends LitElement {
   }
 
   startProgressBarAnimation() {
-    setTimeout(() => {
-      this.progress = this.fillPercentage;
-    }, 100);
+    this.progress = this.fillPercentage;
   }
 
   startCounterAnimation(target) {
@@ -88,8 +89,7 @@ class ProgressBar extends LitElement {
     requestAnimationFrame(animateCounter);
   }
 
-  connectedCallback() {
-    super.connectedCallback();
+  setupIntersectionObserver() {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
@@ -98,6 +98,7 @@ class ProgressBar extends LitElement {
             this.startProgressBarAnimation();
             this.startCounterAnimation(this.fillPercentage);
             observer.unobserve(this);
+            this.requestUpdate();
           }
         });
       },
@@ -107,16 +108,24 @@ class ProgressBar extends LitElement {
     observer.observe(this);
   }
 
+  updated(changedProperties) {
+    if (changedProperties.has('fillPercentage') && !this.isVisible) {
+      this.setupIntersectionObserver();
+    }
+  }
+
   render() {
     return html`
       <div class="progress-bar-wrapper">
         <div class="progress-bar-inner">
-          <div class="progress-bar-filled" style="width:${this.progress}%"></div>
+          <div class="progress-bar-filled" style="width:${this.isVisible ? this.progress : 0}%"></div>
         </div>
-        <div class="counter">${this.counter}s</div>
+        <div class="counter">${this.isVisible ? this.counter : 0}s</div>
       </div>
     `;
   }
-}
+  }
+
+
 
 customElements.define('progress-bar', ProgressBar);
